3929702602d803935748d83ec432427e7b9a8896
ABDELAZIZ AITMOUSSA <57140192+azizou98@users.noreply.github.com>
2024-04-16 14:58:23 +0000

    adding body

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyparser.urlencoded({extended : false}))
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
